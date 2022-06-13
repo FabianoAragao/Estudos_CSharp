c389653569cbb2d3ef8881d3908880a591a2d1e2
Fabiano Malveira Aragao <dev.fabianoaragao@gmail.com>
2022-06-13 13:49:09 -0300

    Crud Scaffolding

---
+++ SalesWebMVC/SalesWebMVC/SalesWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models.ViewModels;
+using System.Diagnostics;
+
+namespace SalesWebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Sales Web MVC app from C# course";
+            ViewData["Professor"] = "Nelio Alves";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
