ec140c95b796d35e06983fa217201082ccb09c83
Fabiano Malveira Aragao <dev.fabianoaragao@gmail.com>
2022-07-04 14:04:02 -0300

    Serviço de seeding

--- SalesWebMVC/SalesWebMVC/SalesWebMVC/Program.cs
+++ SalesWebMVC/SalesWebMVC/SalesWebMVC/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
-var builder = WebApplication.CreateBuilder(args);
+var 
+    builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<SalesWebMVCContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("SalesWebMVCContext") ,
     builder => builder.MigrationsAssembly("SalesWebMVC")));
@@ -11,12 +13,25 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetService<SalesWebMVCContext>();
+
+        ServicoDeSeeding servicoDeSeeding = new ServicoDeSeeding(context);
+
+        servicoDeSeeding.seed();
+    }
 }
 
 app.UseHttpsRedirection();
